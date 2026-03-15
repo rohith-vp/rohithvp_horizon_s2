6a653e1f5d1a4f4a1c5f681ceb06e69f2d9e53c7
Rohith V P <2006rohithvp+github@gmail.com>
2026-03-15 11:03:26 +0530

    first commit

---
+++ level-1-task/part-a.py
@@ -0,0 +1,31 @@
+# Author : Rohith V P
+# Program for calculating ditsance rover must travel
+# when going from (x1,y1) to (x2, y2)
+
+from math import sqrt
+
+
+def distance_calculate():
+
+    try:
+        # Input coordinate values
+        x1 = float(input("Enter x1: "))
+        y1 = float(input("Enter y1: "))
+        x2 = float(input("Enter x2: "))
+        y2 = float(input("Enter y2: "))
+    except ValueError:
+        # If user enters an invalid character
+        print("Invalid value entered")
+        exit()
+
+    # Calculate distance using distance formula
+    d = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    # ROund the floatingpoint
+    d = round(d, 2)
+
+    print(f"Distance the rover must travel: {d} units")
+
+
+# Run only if program is run directly, for modularity
+if __name__ == "__main__":
+    distance_calculate()
